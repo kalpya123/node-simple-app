206fd1cd9adbba9cc5809373d6eb5e714231e51b
kalpya123 <kalpeshdharpure123@gmail.com>
2023-12-26 14:39:47 +0530

    first push

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM node:14-alpine3.16
+
+WORKDIR /app
+
+COPY package.json .
+
+COPY . .
+
+RUN npm install
+
+EXPOSE 3000
+
+CMD [ "npm" ,"start"]
---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express')
+var uniqid = require('uniqid'); 
+const app = express()
+
+app.get('/Docker', function (req, res) {
+  res.status(200).json({"data":[{
+    "id":uniqid(),
+    "name":generateRandomName(),
+  }]})
+})
+
+function generateRandomName() {
+    // Create an array of first names
+    const firstNames = ["Alice", "Bob", "Carol"];
+  
+    // Create an array of last names
+    const lastNames = ["Smith", "Jones", "Williams"];
+  
+    // Generate a random index for the first name array
+    const firstNameIndex = Math.floor(Math.random() * firstNames.length);
+  
+    // Generate a random index for the last name array
+    const lastNameIndex = Math.floor(Math.random() * lastNames.length);
+  
+    // Return a random name
+    return firstNames[firstNameIndex] + " " + lastNames[lastNameIndex];
+  }
+  
+
+app.listen(3000,() => {
+    console.log("Server running on 3000")
+})
